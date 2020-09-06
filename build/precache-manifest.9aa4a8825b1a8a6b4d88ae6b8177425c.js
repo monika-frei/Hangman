2fa302e9db019e27aeeeb0a37e53ea74d0f94a55
Monika <frei.monika@gmail.com>
2020-09-06 14:04:38 +0200

    dodany folder build

---
+++ build/precache-manifest.9aa4a8825b1a8a6b4d88ae6b8177425c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9d22a3067e8af91cd8fd6563a0844c9",
+    "url": "/Hangman/build/index.html"
+  },
+  {
+    "revision": "b6a16ffae8d8ebe1ad52",
+    "url": "/Hangman/build/static/js/2.ac817eec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Hangman/build/static/js/2.ac817eec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c02fd9cf67462cd1787",
+    "url": "/Hangman/build/static/js/main.929f14a7.chunk.js"
+  },
+  {
+    "revision": "b2a0b1c5b1c1aec5f902",
+    "url": "/Hangman/build/static/js/runtime-main.e9a05803.js"
+  }
+]);
